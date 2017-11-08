7d2a4b6aad5a22fbca5ba8d477b0659f59c8185f
shturnev <sht_job@ukr.net>
2017-11-08 15:48:49 +0200

    создал проект настроил composer

---
+++ App/models/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Project local.telegram.
+ * User: sht_j
+ * Date: 08.11.2017
+ * Time: 15:39
+ */
+
+namespace App\models;
+
+
+class Test
+{
+
+    public function __construct()
+    {
+        echo "succes";
+    }
+
+}
